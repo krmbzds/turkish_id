6d87f9cc069771a4e56cbad5fd85befc15d36f5b
Kerem Bozdas <krmbzds.github@gmail.com>
2015-08-27 12:56:40 +0300

    Write specs

--- spec/turkish_id_spec.rb
+++ spec/turkish_id_spec.rb
@@ -5,7 +5,24 @@ describe TurkishId do
     expect(TurkishId::VERSION).not_to be nil
   end
 
-  it 'does something useful' do
-    expect(false).to eq(true)
+  it 'validates a valid id number' do
+    identity_number = TurkishId.new('10000000146')
+    expect(identity_number.is_valid?).to eq(true)
   end
+
+  it 'does not validate invalid id number' do
+    identity_number = TurkishId.new('10001000146')
+    expect(identity_number.is_valid?).to eq(false)
+  end
+
+  it 'does not validate empty string' do
+    identity_number = TurkishId.new('')
+    expect(identity_number.is_valid?).to eq(false)
+  end
+
+  it 'does not validate nil input' do
+    identity_number = TurkishId.new(nil)
+    expect(identity_number.is_valid?).to eq(false)
+  end
+
 end
