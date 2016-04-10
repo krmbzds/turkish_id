5e13e2401b14a5acfdacb24e84a49306fd6b5d9b
Kerem Bozdas <krmbzds.github@gmail.com>
2016-04-10 23:02:28 +0300

    Write specs
    - generates checksum for partial id
    - appends checksum to partial id
    - generates elder relatives
    - generates younger relatives

--- spec/turkish_id_spec.rb
+++ spec/turkish_id_spec.rb
@@ -28,4 +28,28 @@ describe TurkishId do
     expect(identity_number.is_valid?).to eq(false)
   end
 
+  it 'generates checksum for partial id' do
+    partial_id = '100000001'.digitize
+    checksum = TurkishId.get_checksum(partial_id)
+    expect(checksum).to eq([4, 6])
+  end
+
+  it 'appends checksum to partial id' do
+    id = TurkishId.new('10000000146')
+    id_core, result = id.core, '10000000146'.digitize
+    expect(TurkishId.add_checksum(id_core)).to eq(result)
+  end
+
+  it 'generates elder relatives' do
+    id = TurkishId.new('10000000146')
+    result = ["10003000082", "10005999902"]
+    expect(id.get_relatives 2).to eq(result)
+  end
+
+  it 'generates younger relatives' do
+    id = TurkishId.new('10005999902')
+    result = ["10003000082", "10000000146"]
+    expect(id.get_relatives(2, 'down')).to eq(result)
+  end
+
 end
