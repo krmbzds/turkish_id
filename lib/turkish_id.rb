476a573558989d90c0e88ccfda6ff7f00dc5793c
Kerem Bozdas <krmbzds.github@gmail.com>
2018-04-06 16:55:47 +0300

    Refactor code, add new tests, bump version
    - Handle different data types
    - Move digitize into a private method instead of extending String
    - Improve initialize method, keep id and checksum in instance variables
    - Remove find_relatives function (feature creep)

--- lib/turkish_id.rb
+++ lib/turkish_id.rb
@@ -1,78 +1,54 @@
 require "turkish_id/version"
 
-class String
-  def digitize
-    self.split('').map { |s| Integer(s) }
-  end
-end
-
 class TurkishId
+  attr_reader :id_number, :checksum
 
   def initialize(id_number)
-    begin
-      @id_number = id_number.digitize  # Convert string into array of integers
-    rescue
-      @id_number = ''  # Suppress error & return false (eventually)
-    end
-  end
-
-
-  def core
-    @id_number.take(9)
-  end
-
-
-  def self.get_checksum(id_core)
-
-    # Calculate the sums of odd and even digits
-    odds = id_core.values_at(0, 2, 4, 6, 8).reduce(:+)
-    evens = id_core.values_at(1, 3, 5, 7).reduce(:+)
-
-    # Generate checksum digits
-    d10 = ((odds * 7) - evens) % 10
-    d11 = (id_core.reduce(:+) + d10) % 10
-
-    [d10, d11]
-  end
-
-  def self.add_checksum(id_core)
-    id_core + TurkishId.get_checksum(id_core)
+    @id_number = digitize(id_number)
+    @checksum = calculate_checksum(@id_number)
   end
 
   def is_valid?
-    id = @id_number  # +1 brevity point
+    id = @id_number  # For brevity
 
-    # Early elimination, bad length or first digit
+    # Early termination, bad length or first digit
     return false if id.length != 11 || id.first == 0
 
-    # Get checksum digits
-    d10, d11 = TurkishId.get_checksum(id)
+    # Calculate checksum if not already calculated
+    @checksum ||= calculate_checksum(id_number)
 
     # Check if the tenth digit matches the algorithm
-    return false if id[9] != d10
+    return false if id[9] != @checksum[0]
 
     # Check if the eleventh digit matches the algorithm
-    return false if id[10] != d11
+    return false if id[10] != @checksum[1]
 
     true  # All conditions met
   end
 
-  def get_relatives(num, direction='up')
+  private
 
-    id = self.core.join.to_i  # Our core id as integer
+  def calculate_checksum(id_number)
+    # Calculate the sums of odd and even digits
+    odds = id_number.values_at(0, 2, 4, 6, 8).reduce(:+)
+    evens = id_number.values_at(1, 3, 5, 7).reduce(:+)
 
-    tree = {'up' => 1, 'down' => -1}      # + or - based on direction
-    magic = 131 * 229 * tree[direction]   # Eliminates if statement
+    # Generate checksum digits
+    d10 = ((odds * 7) - evens) % 10
+    d11 = (id_number.take(9).reduce(:+) + d10) % 10
 
-    relatives = []
+    # Return checksum
+    [d10, d11]
+  rescue
+    []  # Unable to calculate checksum, initial value remains unchainged
+  end
 
-    num.times do
-      id += magic      # Gives us the next relative
-      relatives << id  # Collecting ids in an array
-    end
+  def digitize(id_number)
+    # Narrow down scope of accepted types
+    return [] unless [String, Integer, Fixnum].include?(id_number.class)
 
-    # Traverse relatives and append checksum digits
-    relatives.map { |r| TurkishId.add_checksum(r.to_s.digitize).join }
+    # Convert string of numbers into an array of integers
+    id_number.to_s.split('').map { |s| Integer(s) }
   end
 
 end
--- spec/turkish_id_spec.rb
+++ spec/turkish_id_spec.rb
@@ -8,13 +8,36 @@ describe TurkishId do
     expect(TurkishId::VERSION).not_to be nil
   end
 
+  it 'accepts integer input' do
+    identity_number = TurkishId.new(10000000146)
+    expect(identity_number.id_number).to eq([1, 0, 0, 0, 0, 0, 0, 0, 1, 4, 6])
+  end
+
+  it 'accepts string input' do
+    identity_number = TurkishId.new('10000000146')
+    expect(identity_number.id_number).to eq([1, 0, 0, 0, 0, 0, 0, 0, 1, 4, 6])
+  end
+
+  it 'generates checksum on initialization' do
+    identity_number = TurkishId.new('10000000146')
+    expect(identity_number.checksum).to eq([4, 6])
+  end
+
   it 'validates a valid id number' do
     identity_number = TurkishId.new('10000000146')
+    expect(identity_number.is_valid?).to eq(true)
+
+    identity_number = TurkishId.new('98655172890')
     expect(identity_number.is_valid?).to eq(true)
   end
 
   it 'does not validate invalid id number' do
     identity_number = TurkishId.new('10001000146')
+    expect(identity_number.is_valid?).to eq(false)
+  end
+
+  it 'does not validate id number that starts with zero' do
+    identity_number = TurkishId.new('00100000146')
     expect(identity_number.is_valid?).to eq(false)
   end
 
@@ -26,30 +49,6 @@ describe TurkishId do
   it 'does not validate nil input' do
     identity_number = TurkishId.new(nil)
     expect(identity_number.is_valid?).to eq(false)
-  end
-
-  it 'generates checksum for partial id' do
-    partial_id = '100000001'.digitize
-    checksum = TurkishId.get_checksum(partial_id)
-    expect(checksum).to eq([4, 6])
-  end
-
-  it 'appends checksum to partial id' do
-    id = TurkishId.new('10000000146')
-    id_core, result = id.core, '10000000146'.digitize
-    expect(TurkishId.add_checksum(id_core)).to eq(result)
-  end
-
-  it 'generates elder relatives' do
-    id = TurkishId.new('10000000146')
-    result = ["10003000082", "10005999902"]
-    expect(id.get_relatives 2).to eq(result)
-  end
-
-  it 'generates younger relatives' do
-    id = TurkishId.new('10005999902')
-    result = ["10003000082", "10000000146"]
-    expect(id.get_relatives(2, 'down')).to eq(result)
   end
 
 end
